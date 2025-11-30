95db2ca6625106c4c833b9751a34da4b587732e1
veercodeprog <veerpratap945050@gmail.com>
2025-11-30 19:16:40 +0530

    updated with token

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const STRAPI_URL = "https://bold-reward-a5d659b526.strapiapp.com/api/articles";
 const STRAPI_TOKEN =
   "826791505ac8852f47ecf59be2f375a3dd86c62f2a6dec6e24d7769f0459e58cdc1daa376bfd9bcb8aa32991a64d58335fd7dcb93060f81cffea3ff256eee706a095058a86f4fa4191470c5aaf6bf9482b8edf6b596ef092e327128871f73aace96a6b3217901db9b797e6cbf6a41ffce2422d6e5ea216d72b51b86c56582286"; // Add your token
-const INTERVAL = 10 * 60 * 1000; // 10 minutes
+const INTERVAL = 5 * 60 * 1000; // 10 minutes
 
 async function keepAlive() {
   try {
